52ebae3c204d43ed3d7ec43b3ddee04da5559927
devcolonel <dev.marioanthony@gmail.com>
2024-02-26 17:19:24 +0300

    update complimentary feeding module action helper

--- opensrp-chw/src/main/java/org/smartregister/chw/actionhelper/ComplimentaryFeedingActionHelper.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/actionhelper/ComplimentaryFeedingActionHelper.java
@@ -2,30 +2,65 @@ package org.smartregister.chw.actionhelper;
 
 import android.content.Context;
 
+import org.json.JSONObject;
+import org.smartregister.chw.R;
 import org.smartregister.chw.anc.actionhelper.HomeVisitActionHelper;
+import org.smartregister.chw.anc.domain.VisitDetail;
 import org.smartregister.chw.anc.model.BaseAncHomeVisitAction;
-import org.smartregister.domain.Alert;
-import org.smartregister.immunization.domain.ServiceWrapper;
+import org.smartregister.chw.util.JsonFormUtils;
+
+import java.text.MessageFormat;
+import java.util.List;
+import java.util.Map;
+
+import timber.log.Timber;
 
 public class ComplimentaryFeedingActionHelper extends HomeVisitActionHelper {
-    Alert alert;
-    Context context;
-    ServiceWrapper serviceWrapper;
+    private Context context;
+    private String complementaryFeedingCounselling = "";
+    private String jsonPayload;
 
-    public ComplimentaryFeedingActionHelper(){}
+    public ComplimentaryFeedingActionHelper(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public void onJsonFormLoaded(String jsonString, Context context, Map<String, List<VisitDetail>> details) {
+        this.context = context;
+        this.jsonPayload = jsonString;
+    }
+
+    @Override
+    public String getPreProcessed() {
+        return super.getPreProcessed();
+    }
 
     @Override
     public void onPayloadReceived(String jsonPayload) {
-        //Update the form before showing to the user
+        try {
+            JSONObject jsonObject = new JSONObject(jsonPayload);
+            complementaryFeedingCounselling = JsonFormUtils.getValue(jsonObject, "comp_feed_counselling_status");
+        } catch (Exception e) {
+            Timber.e(e);
+        }
     }
 
     @Override
     public String evaluateSubTitle() {
-        return "";
+        if (!complementaryFeedingCounselling.isEmpty()) {
+            return MessageFormat.format(context.getString(R.string.counselled_mother_for_comp_feeding) + " : {0}", complementaryFeedingCounselling.equals("yes") ? context.getString(R.string.yes) : context.getString(R.string.no));
+        } else {
+            return "";
+        }
     }
 
     @Override
     public BaseAncHomeVisitAction.Status evaluateStatusOnPayload() {
-        return BaseAncHomeVisitAction.Status.COMPLETED;
+        if (complementaryFeedingCounselling.equals("yes"))
+            return BaseAncHomeVisitAction.Status.COMPLETED;
+        else if (complementaryFeedingCounselling.equals("no"))
+            return BaseAncHomeVisitAction.Status.PARTIALLY_COMPLETED;
+        else
+            return BaseAncHomeVisitAction.Status.PENDING;
     }
 }
