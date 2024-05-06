49a87054842f9299e43524c7f0238ba3f7801a6f
issyzac <issyzac.iz@gmail.com>
2024-05-07 00:10:16 +0300

    :zap: add addo linkage register on navigation menu

--- opensrp-chw/src/nacp/java/org/smartregister/chw/model/NavigationModelFlv.java
+++ opensrp-chw/src/nacp/java/org/smartregister/chw/model/NavigationModelFlv.java
@@ -1,5 +1,6 @@
 package org.smartregister.chw.model;
 
+import static org.smartregister.AllConstants.CHECK_BOX;
 import static org.smartregister.AllConstants.TEAM_ROLE_IDENTIFIER;
 
 import android.content.SharedPreferences;
@@ -48,6 +49,8 @@ public class NavigationModelFlv implements NavigationModel.Flavor {
             NavigationOption op21 = new NavigationOption(R.mipmap.sidemenu_updates, R.mipmap.sidemenu_updates_active, R.string.sbc, CoreConstants.DrawerMenu.SBC, 0);
             NavigationOption op22 = new NavigationOption(R.mipmap.sidemenu_updates, R.mipmap.sidemenu_updates_active, R.string.sbc_monthly_social_media_report, CoreConstants.DrawerMenu.SBC_MONTHLY_SOCIAL_MEDIA_REPORT, 0);
 
+            NavigationOption op23 = new NavigationOption(R.mipmap.sidemenu_referrals, R.mipmap.sidemenu_referrals_active, R.string.nav_menu_linkage, CoreConstants.DrawerMenu.ADDO_LINKAGE, 0);
+
             AllSharedPreferences allSharedPreferences = Utils.getAllSharedPreferences();
             SharedPreferences preferences = allSharedPreferences.getPreferences();
             String teamRoleIdentifier = "";
@@ -87,6 +90,9 @@ public class NavigationModelFlv implements NavigationModel.Flavor {
                             navigationOptions.add(op22);
                             navigationOptions.add(op21);
                         }
+                        if (ChwApplication.getApplicationFlavor().hasADDO()){
+                            navigationOptions.add(op23);
+                        }
 
                         navigationOptions.addAll(Arrays.asList(op8, op15));
                         break;
@@ -111,6 +117,9 @@ public class NavigationModelFlv implements NavigationModel.Flavor {
                 if (ChwApplication.getApplicationFlavor().hasSbc()) {
                     navigationOptions.add(op22);
                     navigationOptions.add(op21);
+                }
+                if (ChwApplication.getApplicationFlavor().hasADDO()){
+                    navigationOptions.add(op23);
                 }
                 navigationOptions.addAll(Arrays.asList(op8, op15));
             }
