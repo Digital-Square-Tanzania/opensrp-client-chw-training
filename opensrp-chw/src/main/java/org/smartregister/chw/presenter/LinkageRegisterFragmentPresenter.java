600a5ccc9b1db932c97050726bdea7410d5e6564
issyzac <issyzac.iz@gmail.com>
2024-06-12 15:39:11 +0300

    :recycle: update main condition to list linkages

--- opensrp-chw/src/main/java/org/smartregister/chw/presenter/LinkageRegisterFragmentPresenter.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/presenter/LinkageRegisterFragmentPresenter.java
@@ -8,6 +8,7 @@ import org.smartregister.chw.referral.contract.BaseReferralRegisterFragmentContr
 import org.smartregister.chw.referral.presenter.BaseReferralRegisterFragmentPresenter;
 import org.smartregister.chw.referral.util.DBConstants;
 import org.smartregister.chw.util.Constants;
+import org.smartregister.domain.Task;
 
 /**
  * Author issyzac on 07/05/2024
@@ -23,8 +24,8 @@ public class LinkageRegisterFragmentPresenter extends BaseReferralRegisterFragme
     public String getMainCondition() {
         return " " + Constants.TABLE_NAME.FAMILY_MEMBER + "." + DBConstants.Key.DATE_REMOVED + " is null " +
                 "AND " + org.smartregister.chw.referral.util.Constants.Tables.REFERRAL + "." + DBConstants.Key.REFERRAL_TYPE + " = '" + org.smartregister.chw.referral.util.Constants.ReferralType.COMMUNITY_TO_ADDO_REFERRAL + "' " +
-                "AND " + CoreConstants.TABLE_NAME.TASK + "." + ChwDBConstants.TaskTable.STATUS + " <> '" + CoreConstants.BUSINESS_STATUS.CANCELLED + "' " +
-                "AND " + CoreConstants.TABLE_NAME.TASK + "." + ChwDBConstants.TaskTable.STATUS + " <> 'COMPLETED' ";
+                "AND " + CoreConstants.TABLE_NAME.TASK + "." + ChwDBConstants.TaskTable.STATUS + " <> '" + Task.TaskStatus.CANCELLED + "' " +
+                "AND " + CoreConstants.TABLE_NAME.TASK + "." + ChwDBConstants.TaskTable.STATUS + " <> '"+Task.TaskStatus.COMPLETED+"'";
 
     }
 
