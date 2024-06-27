c7916777195669e5da85d96ea20fca8fce3391de
Kassim Sheghembe <sheghembekassim@gmail.com>
2024-06-27 14:43:21 +0300

    Create referral event and referral tasks when saving ANC home visit

--- opensrp-chw/src/main/java/org/smartregister/chw/interactor/AncHomeVisitInteractor.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/interactor/AncHomeVisitInteractor.java
@@ -1,9 +1,45 @@
 package org.smartregister.chw.interactor;
 
+import android.content.Context;
+
+import org.smartregister.chw.R;
+import org.smartregister.chw.anc.model.BaseAncHomeVisitAction;
 import org.smartregister.chw.core.interactor.CoreAncHomeVisitInteractor;
+import org.smartregister.chw.core.utils.CoreConstants;
+import org.smartregister.chw.util.ReferralUtils;
+
+import java.util.Map;
 
 public class AncHomeVisitInteractor extends CoreAncHomeVisitInteractor {
-    public AncHomeVisitInteractor() {
+
+    Context context;
+    public AncHomeVisitInteractor(Context context) {
+        this.context = context;
         setFlavor(new AncHomeVisitInteractorFlv());
     }
+
+    @Override
+    protected void submitVisit(boolean editMode, String memberID, Map<String, BaseAncHomeVisitAction> map, String parentEventType) throws Exception {
+        if (map != null && !editMode) {
+            BaseAncHomeVisitAction facilitySelectionAction = map.get(context.getString(R.string.home_visit_facility_referral));
+            if (facilitySelectionAction != null) {
+                String facilitySelectionForm = facilitySelectionAction.getJsonPayload();
+                if (facilitySelectionForm != null) {
+
+                    BaseAncHomeVisitAction dangerSignsActions = map.get(context.getString(R.string.anc_home_visit_danger_signs));
+
+                    try {
+
+                        // Here pass the  dangerSignsSelectedObject to the processReferral method and then add the problems to the referralProblems
+                        ReferralUtils.processReferral(facilitySelectionForm, memberID, CoreConstants.TASKS_FOCUS.ANC_DANGER_SIGNS, dangerSignsActions.getJsonPayload());
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+        }
+        super.submitVisit(editMode, memberID, map, parentEventType);
+    }
+
 }
