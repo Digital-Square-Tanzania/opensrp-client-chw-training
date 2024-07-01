64eb8274ddf54315ecd47288a220a5decdddd54a
Kassim Sheghembe <sheghembekassim@gmail.com>
2024-07-01 12:35:47 +0300

    Refactor ChildHomeVisit to save referral event and create referral tasks.

--- opensrp-chw/src/main/java/org/smartregister/chw/activity/ChildHomeVisitActivity.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/activity/ChildHomeVisitActivity.java
@@ -1,13 +1,21 @@
 package org.smartregister.chw.activity;
 
+import org.json.JSONObject;
+import org.smartregister.chw.R;
 import org.smartregister.chw.anc.model.BaseAncHomeVisitAction;
 import org.smartregister.chw.anc.presenter.BaseAncHomeVisitPresenter;
 import org.smartregister.chw.anc.util.AppExecutors;
 import org.smartregister.chw.core.activity.CoreChildHomeVisitActivity;
 import org.smartregister.chw.core.interactor.CoreChildHomeVisitInteractor;
+import org.smartregister.chw.core.utils.CoreConstants;
 import org.smartregister.chw.interactor.ChildHomeVisitInteractorFlv;
+import org.smartregister.chw.util.JsonFormUtils;
+import org.smartregister.chw.util.ReferralUtils;
 
 import java.util.LinkedHashMap;
+import java.util.Map;
+
+import timber.log.Timber;
 
 public class ChildHomeVisitActivity extends CoreChildHomeVisitActivity {
     @Override
@@ -19,6 +27,38 @@ public class ChildHomeVisitActivity extends CoreChildHomeVisitActivity {
     public void submittedAndClose() {
         super.submittedAndClose();
         ChildProfileActivity.startMe(this, memberObject, ChildProfileActivity.class);
+    }
+
+    @Override
+    public void submitVisit() {
+        super.submitVisit();
+
+        Map<String, BaseAncHomeVisitAction> actions = this.getAncHomeVisitActions();
+        if (actions !=  null) {
+
+            BaseAncHomeVisitAction facilitySelectionAction = actions.get(this.getString(R.string.home_visit_facility_referral));
+
+            String facilitySelectionForm = facilitySelectionAction.getJsonPayload();
+            if (facilitySelectionForm != null) {
+
+                BaseAncHomeVisitAction dangerSignsActions = actions.get(this.getString(R.string.child_danger_signs_baby));
+
+                try {
+
+                    assert dangerSignsActions != null;
+                    String dangerSignsForm = dangerSignsActions.getJsonPayload();
+                    assert dangerSignsForm != null;
+                    JSONObject dangerSignsJsonObject = new JSONObject(dangerSignsForm);
+
+                    String referralProblems = JsonFormUtils.getCheckBoxValue(dangerSignsJsonObject, "toddler_danger_signs_present");
+                    ReferralUtils.processReferral(facilitySelectionForm, this.memberObject.getBaseEntityId(), CoreConstants.TASKS_FOCUS.SICK_CHILD, referralProblems);
+                } catch (Exception e) {
+                    Timber.e(e);
+                }
+
+            }
+        }
+
     }
 
     @Override
