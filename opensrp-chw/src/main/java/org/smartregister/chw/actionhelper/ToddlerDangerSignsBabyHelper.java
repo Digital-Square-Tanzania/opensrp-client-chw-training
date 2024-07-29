f08973441f23d45c585b24c46db13efe30a092ac
nitusima <nkataraia@d-tree.org>
2024-07-29 15:35:11 +0300

    Making Danger mandatory before other actions

--- opensrp-chw/src/main/java/org/smartregister/chw/actionhelper/ToddlerDangerSignsBabyHelper.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/actionhelper/ToddlerDangerSignsBabyHelper.java
@@ -11,22 +11,28 @@ import org.json.JSONObject;
 import org.smartregister.chw.R;
 import org.smartregister.chw.anc.actionhelper.HomeVisitActionHelper;
 import org.smartregister.chw.anc.model.BaseAncHomeVisitAction;
+import org.smartregister.chw.util.FnInterfaces;
 import org.smartregister.domain.Alert;
 
 import java.text.MessageFormat;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Future;
 
 import timber.log.Timber;
 
 public class ToddlerDangerSignsBabyHelper extends HomeVisitActionHelper {
-    private String danger_signs_present_child;
+    private String dangerSignPresentChild;
+    private final FnInterfaces.Action<Boolean> nonDangerSignsEvaluator;
 
     private final Context context;
 
     private final Alert alert;
 
-    public ToddlerDangerSignsBabyHelper(Context context, Alert alert){
+
+    public ToddlerDangerSignsBabyHelper(Context context, Alert alert, FnInterfaces.Action<Boolean> evaluator) {
         this.context = context;
         this.alert = alert;
+        nonDangerSignsEvaluator=evaluator;
     }
 
     @Override
@@ -42,7 +48,7 @@ public class ToddlerDangerSignsBabyHelper extends HomeVisitActionHelper {
     public void onPayloadReceived(String s) {
         try {
             JSONObject jsonObject = new JSONObject(s);
-            danger_signs_present_child = getCheckBoxValue(jsonObject, "toddler_danger_signs_present");
+            dangerSignPresentChild = getCheckBoxValue(jsonObject, "toddler_danger_signs_present");
         } catch (JSONException e) {
             Timber.e(e);
         }
@@ -50,24 +56,28 @@ public class ToddlerDangerSignsBabyHelper extends HomeVisitActionHelper {
 
     @Override
     public String evaluateSubTitle() {
-        return MessageFormat.format("{0}: {1}", context.getString(R.string.child_danger_signs_baby_task), danger_signs_present_child);
+        return MessageFormat.format("{0}: {1}", context.getString(R.string.child_danger_signs_baby_task), dangerSignPresentChild);
     }
 
     @Override
     public String postProcess(String jsonPayload) {
+        boolean noDangerSigns=dangerSignPresentChild.matches("(?i)none|hakuna");
+            try {nonDangerSignsEvaluator.apply(noDangerSigns);}
+            catch (Exception e) {Timber.e(e);}
         return super.postProcess(jsonPayload);
     }
 
     @Override
     public BaseAncHomeVisitAction.Status evaluateStatusOnPayload() {
-        if (StringUtils.isBlank(danger_signs_present_child)) {
+        if (StringUtils.isBlank(dangerSignPresentChild)) {
             return BaseAncHomeVisitAction.Status.PENDING;
         }
 
-        if (StringUtils.isNotBlank(danger_signs_present_child)) {
+        if (StringUtils.isNotBlank(dangerSignPresentChild)) {
             return BaseAncHomeVisitAction.Status.COMPLETED;
         } else {
             return BaseAncHomeVisitAction.Status.PARTIALLY_COMPLETED;
         }
     }
+
 }
