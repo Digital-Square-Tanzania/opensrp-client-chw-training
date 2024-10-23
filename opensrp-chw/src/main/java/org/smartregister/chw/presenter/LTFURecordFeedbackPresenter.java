8d9b2a4e4958ee315814f091edd9fe3de4488440
ChrissDisigale <chrispro.genius@gmail.com>
2024-10-23 16:51:27 +0300

    added service and prescription on referral detail view

--- opensrp-chw/src/main/java/org/smartregister/chw/presenter/LTFURecordFeedbackPresenter.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/presenter/LTFURecordFeedbackPresenter.java
@@ -79,24 +79,24 @@ public class LTFURecordFeedbackPresenter extends BaseIssueReferralPresenter {
         }
     }
 
-    @Override
-    public void saveForm(@NonNull HashMap<String, NFormViewData> valuesHashMap, @NonNull JSONObject jsonObject) {
-        //first close the referral task
-        //if from the valuesHasMap followupStatus value is client_found_ready_to_return then call save form super
-        //else create an event that just sends the feedback to the server
-        if (StringUtils.containsIgnoreCase(String.valueOf(valuesHashMap.get("followup_status").getValue()), "client_found_ready_to_return")) {
-            tagWithReferralDetails(valuesHashMap);
-            super.saveForm(valuesHashMap, jsonObject);
-        }
-        try {
-            createFeedbackEvent(valuesHashMap);
-        } catch (Exception e) {
-            Timber.e(e);
-        }
-        saveCloseReferralEvent();
-        completeTask();
-
-    }
+//    @Override
+//    public void saveForm(@NonNull HashMap<String, NFormViewData> valuesHashMap, @NonNull JSONObject jsonObject) {
+//        //first close the referral task
+//        //if from the valuesHasMap followupStatus value is client_found_ready_to_return then call save form super
+//        //else create an event that just sends the feedback to the server
+//        if (StringUtils.containsIgnoreCase(String.valueOf(valuesHashMap.get("followup_status").getValue()), "client_found_ready_to_return")) {
+//            tagWithReferralDetails(valuesHashMap);
+//            super.saveForm(valuesHashMap, jsonObject);
+//        }
+//        try {
+//            createFeedbackEvent(valuesHashMap);
+//        } catch (Exception e) {
+//            Timber.e(e);
+//        }
+//        saveCloseReferralEvent();
+//        completeTask();
+//
+//    }
 
     private void tagWithReferralDetails(HashMap<String, NFormViewData> valuesHashMap) {
 
